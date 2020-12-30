4552a3093d7124e3a38ccaf7e59e462e860a89ff
Arrakis <nowfel.terki@gmail.com>
2020-12-30 16:28:12 +0100

    Add methods to upper and lower case.

--- src/TextFoundation.php
+++ src/TextFoundation.php
@@ -4,5 +4,16 @@ namespace Nowfel\TextFoundationBundle;
 
 class TextFoundation
 {
+    /**
+     * Convert string to an uppercase.
+     */
+    public function toUpperCase(string $text): string
+    {
+        return strtoupper($text);
+    }
 
+    public function toLowerCase(string $text): string
+    {
+        return strtolower($text);
+    }
 }
