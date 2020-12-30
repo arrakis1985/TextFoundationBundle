a133b860aea6981504e9b6b8c5e69875bd41633b
Nowfel Terki <nowfel.terki@tutanota.com>
2020-12-30 12:14:13 +0100

    Initial commit

---
+++ src/TextFoundation.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Nowfel\TextFoundationBundle;
+
+class TextFoundation
+{
+
+}
---
+++ src/TextFoundationBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Nowfel\TextFoundationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TextFoundationBundle extends Bundle
+{
+
+}
